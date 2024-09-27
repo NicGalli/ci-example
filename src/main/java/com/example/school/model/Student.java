2693c5befcaea3faaa40d44cadb9b83413fa0942
NicGalli <nikk094@live.it>
2024-09-27 17:58:52 +0200

    implementation

---
+++ src/main/java/com/example/school/model/Student.java
@@ -0,0 +1,20 @@
+package com.example.school.model;
+
+public class Student {
+	String id;
+	String name;
+
+	public Student() {
+
+	}
+
+	public Student(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+}
